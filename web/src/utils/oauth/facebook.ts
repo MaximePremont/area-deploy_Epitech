d59ebe062e787d37144d7bc25ce8d9177189cc2b
Nolann <62215577+Nolann71@users.noreply.github.com>
2023-03-07 18:13:08 +0100

    hot: fix facebook

--- web/src/utils/oauth/facebook.ts
+++ web/src/utils/oauth/facebook.ts
@@ -3,7 +3,7 @@ export const getOauthConnectFacebookUrl = () => {
 
   const options = {
     redirect_uri: import.meta.env.VITE_FACEBOOK_OAUTH_CONNECT_REDIRECT_URL,
-    client_id: import.meta.env.VITE_FACEBOOK_OAUTH_CONNECT_CLIENT_ID,
+    client_id: import.meta.env.VITE_FACEBOOK_OAUTH_CLIENT_ID,
     response_type: 'code',
     scope: 'email public_profile user_friends',
   }
